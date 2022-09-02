05b0ff640fe9408a8b838368d9d6c17d0c71f325
Anastasia <cookiedemons@outlook.com>
2022-09-01 22:51:10 -0400

    images 'placeholder' (jsp la traduction)

--- js/MenuPrincipal.js
+++ js/MenuPrincipal.js
@@ -1,6 +1,8 @@
 function mouseIsOverMainMenuButton(){
-    document.getElementById("startButtonImage").src= "../images/george.png";
+    document.getElementById("startButtonImage").src= "../images/greenStartButton.png";
+    document.getElementById("backgroundMainMenu").style.backgroundImage = "url(../images/hellfire_realm.jpg)";
 }
 function mouseIsOutOfMainMenuButton(){
     document.getElementById("startButtonImage").src= "../images/start.png";
+    document.getElementById("backgroundMainMenu").style.backgroundImage = "url(../images/castle.jpg)";
 }
